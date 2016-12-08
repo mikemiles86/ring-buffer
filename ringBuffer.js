69832c5ed57a08b8dba9efb776aa7878a1adfb6b
Mike Miles <MMiles@genuineinteractive.com>
2016-12-08 11:59:00 -0500

    comment new code

--- ringBuffer.js
+++ ringBuffer.js
@@ -1,3 +1,75 @@
+/*
+function RingBuffer(size) {
+  var bufferSize = size,
+      buffer = new Array(size),
+      readers = [],
+      bufferRead = bufferWrite = bufferCount = 0
+  this.getBuffer = function() { return buffer }
+  this.getRead = function() { return bufferRead }
+  this.read = function() {
+    var data = buffer[bufferRead]
+    buffer[bufferRead] = null
+    bufferRead = this.getNextPointer(bufferRead)
+    --bufferCount
+    return data
+  }
+  this.readBuffer = function(pointer) {
+    return buffer[pointer];
+  }
+  this.write = function(data) {
+    buffer[bufferWrite] = data
+    var len = readers.length;
+    if (len > 0) {
+      for (var r = 0; r < len; ++r) {
+        readers[r].removeRead(bufferWrite)
+      }
+    }
+    if ((bufferWrite == bufferRead) && (buffer[bufferRead+1] != null)) {
+      bufferRead = this.getNextPointer(bufferRead)
+    }
+    bufferWrite = this.getNextPointer(bufferWrite)
+    ++bufferCount
+  }
+  this.count = function() {
+    return bufferCount
+  }
+  this.join = function() {
+    var reader = new BufferReader(this)
+    readers[readers.length] = reader
+    return reader
+  }
+  this.getNextPointer = function(current_pointer) {
+    return (current_pointer >= (bufferSize-1)) ? 0:(current_pointer+1)
+  }
+}
+
+function BufferReader(ring_buffer) {
+  var ringBuffer = ring_buffer,
+      readIndex = ring_buffer.getRead,
+      hasRead = new Array();
+  this.getBuffer = function() { return ringBuffer.getBuffer()}
+  this.read = function() {
+    var data =  null
+    if (hasRead.indexOf(readIndex) < 0) {
+      data = ringBuffer.readBuffer(readIndex);
+      hasRead[hasRead.length] = readIndex;
+      readIndex = ringBuffer.getNextPointer(readIndex);
+    }
+    return data;
+  }
+  this.removeRead = function(removeIndex) {
+    var index = hasRead.indexOf(removeIndex)
+    if (index >= 0) {
+      for(var i = index, len = hasRead.length -1; i < len; ++i) {
+        hasRead[i] = hasRead[i + 1]
+      }
+      hasRead.length = len;
+    }
+  }
+}
+*/
+
+
 var RingBuffer = function(size) {
   this.bufferSize = size
   this.buffer = new Array(size)
@@ -66,5 +138,4 @@ BufferReader.prototype.removeRead = function(removeIndex) {
     this.hasRead.length = len;
   }
 }
-
 
